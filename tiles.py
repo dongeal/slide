dd00556200913f1364ece522d4c192d3c3d0f5d1
DESKTOP-OK2D2LF\donge <dongeal@gmail.com>
2023-06-24 21:34:02 +0900

    game over 만 만들면 될듯.... 마우스다운 무브함수 체크가 비효율적... 너무 큼,,,   글자 센터에 넣는법 공부해야 합

--- tiles.py
+++ tiles.py
@@ -4,16 +4,19 @@ from random import  shuffle
 pg.init()           # 2. 초기화
 
 
-
-
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (128,128,128)
-CELL_SIZE = 200
-COLUMN_COUNT =3
-ROW_COUNT = 3
-SCREEN_WIDTH = CELL_SIZE * ROW_COUNT
-SCREEN_HEIGHT = CELL_SIZE * COLUMN_COUNT
+CELL_SIZE = 100
+BOARD_SIZE_X = 3
+BOARD_SIZE_Y = 3
+
+SCREEN_WIDTH = CELL_SIZE * BOARD_SIZE_X
+SCREEN_HEIGHT = CELL_SIZE * BOARD_SIZE_Y
+# 폰트 정의
+game_font = pg.font.Font(None, CELL_SIZE) # 폰트 객체 생성 (폰트, 크기)
+
+
 
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #화면 크기 설정
 pg.display.set_caption('Slide')
@@ -22,50 +25,108 @@ clock = pg.time.Clock()
 done = False
 
 # 타일 랜덤 작업
-# tiles = []
-# for index in range(0, COLUMN_COUNT * ROW_COUNT):
-#     tiles.append(index)
+tiles = []
+for index in range(0, BOARD_SIZE_X * BOARD_SIZE_Y):
+    tiles.append(index)  # [0, 1, 2, ....., 15]
 
-tiles =[x for x in range(COLUMN_COUNT * ROW_COUNT)] 
-tile_colors = {0:BLACK}
-for index in range(0, COLUMN_COUNT * ROW_COUNT):
-    tile_colors[index]= WHITE
-
-
-
-
-print(tiles)
+# tiles =[x for x in range(COLUMN_COUNT * BOARD_SIZE_X)] 
+# tile_colors = {0:BLACK}
+# for index in range(0, COLUMN_COUNT * BOARD_SIZE_X):
+#     tile_colors[index]= WHITE
 shuffle(tiles)
-print(tiles)
 
 class Tiles :
-    def __init__(self,tile_no, row_pos, col_pos):
+    def __init__(self, tile_no, x_pos, y_pos):
         self.tile_no = tile_no
-        self.row_pos = row_pos
-        self.col_pos = col_pos
+        self.x_pos = x_pos
+        self.y_pos = y_pos
        
-    def draw_black(self):
-        if self.tile_no ==0 :
-            pg.draw.rect(screen, BLACK,
-                        (self.row_pos * CELL_SIZE, self.col_pos * CELL_SIZE,
-                          CELL_SIZE,CELL_SIZE))
-    def draw(self,color):
-        pg.draw.rect(screen, color , pg.Rect
-                     (self.row_pos * CELL_SIZE+1, self.col_pos * CELL_SIZE+1,
-                       CELL_SIZE-1, CELL_SIZE-1))
+    def draw(self, color):
+        # print(color, self.tile_no, self.x_pos, self.y_pos)
+        pg.draw.rect(screen, color,
+                 (self.x_pos * CELL_SIZE, self.y_pos * CELL_SIZE,
+                   CELL_SIZE,CELL_SIZE))    
+        NO_TAG = game_font.render(str(tiles[self.tile_no]), True, BLACK)
+                    # 출력할 글자, 안티 알리아스  True, 글자 색상
+        screen.blit(NO_TAG, (int((self.x_pos +0.2) * CELL_SIZE),int((self.y_pos+0.2) * CELL_SIZE)))
+        # # print(NO_TAG, self.x_pos,self.y_pos)
             
-    def move(self):
-       
-        print (self.tile_no, self.row_pos, self.col_pos)    
-        
-        
+    def move(self, tile_no):
 
+        if tiles[tile_no] != 0 : 
+            #top left
+            if self.x_pos == 0 and self.y_pos == 0 :     
+                if tiles[tile_no+1] == 0:                     # 오른쪽 0  
+                    tiles[tile_no],tiles[tile_no+1]= tiles[tile_no+1],tiles[tile_no] 
+                elif tiles[tile_no + BOARD_SIZE_X] == 0 :           # 아래 0
+                    tiles[tile_no], tiles[tile_no + BOARD_SIZE_X] = tiles[tile_no + BOARD_SIZE_X], tiles[tile_no]
+            #top
+            elif self.x_pos > 0 and self.x_pos < BOARD_SIZE_X -1 and self.y_pos == 0:
+                if tiles[tile_no-1] == 0:        # 왼쪽   0
+                     tiles[tile_no],tiles[tile_no-1]= tiles[tile_no-1],tiles[tile_no]
+                elif tiles[tile_no+BOARD_SIZE_X] == 0 :           # 아래 0
+                    tiles[tile_no],tiles[tile_no+BOARD_SIZE_X]= tiles[tile_no+BOARD_SIZE_X],tiles[tile_no]
+                elif tiles[tile_no+1] == 0:                     # 오른쪽 0  
+                    tiles[tile_no],tiles[tile_no+1]= tiles[tile_no+1],tiles[tile_no]
+            #top right
+            if self.x_pos == BOARD_SIZE_X-1 and self.y_pos == 0 :     
+                if tiles[tile_no-1] == 0:                     # 왼쪽 0  
+                    tiles[tile_no],tiles[tile_no-1]= tiles[tile_no-1],tiles[tile_no]
+                elif tiles[tile_no+BOARD_SIZE_X] == 0 :           # 아래 0
+                    tiles[tile_no],tiles[tile_no+BOARD_SIZE_X]= tiles[tile_no+BOARD_SIZE_X],tiles[tile_no]
+            #left
+            elif self.x_pos == 0  and self.y_pos > 0 and self.y_pos < BOARD_SIZE_Y -1:
+                if tiles[tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
+                     tiles[tile_no],tiles[tile_no-BOARD_SIZE_X]= tiles[tile_no-BOARD_SIZE_X],tiles[tile_no]
+                elif tiles[tile_no+BOARD_SIZE_X] == 0 :           # 아래 0
+                    tiles[tile_no],tiles[tile_no+BOARD_SIZE_X]= tiles[tile_no+BOARD_SIZE_X],tiles[tile_no]
+                elif tiles[tile_no+1] == 0:                     # 오른쪽 0  
+                    tiles[tile_no],tiles[tile_no+1]= tiles[tile_no+1],tiles[tile_no]
+            #center
+            elif self.x_pos > 0 and self.x_pos < BOARD_SIZE_X -1 and self.y_pos > 0 and self.y_pos < BOARD_SIZE_Y-1 :
+                if tiles[tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
+                     tiles[tile_no],tiles[tile_no-BOARD_SIZE_X]= tiles[tile_no-BOARD_SIZE_X],tiles[tile_no]
+                elif tiles[tile_no+BOARD_SIZE_X] == 0 :           # 아래 0
+                    tiles[tile_no],tiles[tile_no+BOARD_SIZE_X]= tiles[tile_no+BOARD_SIZE_X],tiles[tile_no]
+                elif tiles[tile_no+1] == 0:                     # 오른쪽 0  
+                    tiles[tile_no],tiles[tile_no+1]= tiles[tile_no+1],tiles[tile_no]    
+                elif tiles[tile_no-1] == 0:                     # 왼쪽 0  
+                    tiles[tile_no],tiles[tile_no-1]= tiles[tile_no-1],tiles[tile_no]    
+            #right
+            elif self.x_pos == BOARD_SIZE_X -1 and self.y_pos > 0 and self.y_pos < BOARD_SIZE_Y-1 :
+                if tiles[tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
+                     tiles[tile_no],tiles[tile_no-BOARD_SIZE_X]= tiles[tile_no-BOARD_SIZE_X],tiles[tile_no]
+                elif tiles[tile_no+BOARD_SIZE_X] == 0 :           # 아래 0
+                    tiles[tile_no],tiles[tile_no+BOARD_SIZE_X]= tiles[tile_no+BOARD_SIZE_X],tiles[tile_no]
+                elif tiles[tile_no-1] == 0:                     # 왼쪽 0  
+                    tiles[tile_no],tiles[tile_no-1]= tiles[tile_no-1],tiles[tile_no]    
+            #bottom left
+            elif self.x_pos  ==0  and self.y_pos == BOARD_SIZE_Y-1 :
+                if tiles[tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
+                     tiles[tile_no],tiles[tile_no-BOARD_SIZE_X]= tiles[tile_no-BOARD_SIZE_X],tiles[tile_no]
+                elif tiles[tile_no+1] == 0:                     # 오른쪽 0  
+                    tiles[tile_no],tiles[tile_no+1]= tiles[tile_no+1],tiles[tile_no]     
+            #bottom
+            elif self.x_pos > 0 and self.x_pos < BOARD_SIZE_X -1 and self.y_pos == BOARD_SIZE_Y-1 :
+                if tiles[tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
+                     tiles[tile_no],tiles[tile_no-BOARD_SIZE_X]= tiles[tile_no-BOARD_SIZE_X],tiles[tile_no]
+                elif tiles[tile_no+1] == 0:                     # 오른쪽 0  
+                    tiles[tile_no],tiles[tile_no+1]= tiles[tile_no+1],tiles[tile_no]    
+                elif tiles[tile_no-1] == 0:                     # 왼쪽 0  
+                    tiles[tile_no],tiles[tile_no-1]= tiles[tile_no-1],tiles[tile_no]
+            #bottom right
+            elif self.x_pos== BOARD_SIZE_X -1 and self.y_pos == BOARD_SIZE_Y-1 :
+                if tiles[tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
+                     tiles[tile_no],tiles[tile_no-BOARD_SIZE_X]= tiles[tile_no-BOARD_SIZE_X],tiles[tile_no]
+                elif tiles[tile_no-1] == 0:                     # 왼쪽 0  
+                    tiles[tile_no],tiles[tile_no-1]= tiles[tile_no-1],tiles[tile_no]    
+          
 
 def draw_grid():
-        for x in range(ROW_COUNT):
-            for y in range(COLUMN_COUNT):
+        for x in range(BOARD_SIZE_X):
+            for y in range(BOARD_SIZE_Y):
                 pg.draw.rect(screen, GRAY,
-                        (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE,CELL_SIZE), 1)
+                        (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE,CELL_SIZE), 2)
                 
 
 def runGame():
@@ -79,26 +140,37 @@ def runGame():
             if event.type == pg.QUIT:
                 done = True
             elif event.type == pg.MOUSEBUTTONDOWN:
-                row_pos = event.pos[0] // CELL_SIZE
-                col_pos = event.pos[1] // CELL_SIZE
-                tile_no = tiles[row_pos + col_pos * COLUMN_COUNT]
-                tile= Tiles(tile_no, row_pos, col_pos)
-                tile.draw_black()
-                tile.move()
+                x_pos = event.pos[0] // CELL_SIZE
+                y_pos = event.pos[1] // CELL_SIZE
+                tile_no = x_pos + y_pos * BOARD_SIZE_Y
+                tile= Tiles(tile_no, x_pos, y_pos)
+                
+                # print (tiles)
 
-            for x in range(len(tiles)):
-              
-                tile_no= tiles[x]
-                color = tiles[tile_no]
-                row_pos = x *  CELL_SIZE % ROW_COUNT
-                col_pos = x * CELL_SIZE // COLUMN_COUNT
-                tile= Tiles(tile_no, row_pos, col_pos)
-                tile.draw(color) 
-                # print (row_pos, col_pos)
+                tile.move(tile_no)
+                
+                # print (tiles)
 
-        draw_grid()
-       
-        pg.display.update()
+                for index in range(len(tiles)):
+                
+                    tile_no= tiles[index]
+                    # print( tile_no)
+                    x_pos = index  % BOARD_SIZE_X
+                    y_pos = index  // BOARD_SIZE_Y
+                    tile= Tiles(index, x_pos, y_pos)
+                    if tile_no == 0:
+                        color = BLACK
+                    else :
+                        color = WHITE    
+                    tile.draw(color) 
+               
+                # print (x_pos, y_pos)
+            # print('////////////////') 
+            # print(tiles)   
+            # print(len(tiles))
+                    draw_grid()
+                pg.display.update()
             
-runGame()
-pg.quit()
+if __name__ == '__main__':                  
+    runGame()
+    pg.quit()
