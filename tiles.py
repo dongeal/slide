2bed66b5d398df953fa948783c230c3c664ce6c8
dongeal <dongeal@gmail.com>
2023-06-26 12:06:23 +0900

    win은 해결 됬는데  poor  해결 해야 함.

--- tiles.py
+++ tiles.py
@@ -7,6 +7,7 @@ pg.init()           # 2. 초기화
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (128,128,128)
+RED = (255,0,0)
 CELL_SIZE = 100
 BOARD_SIZE_X = 3
 BOARD_SIZE_Y = 3
@@ -36,90 +37,110 @@ for index in range(0, BOARD_SIZE_X * BOARD_SIZE_Y):
 shuffle(tiles)
 
 class Tiles :
-    def __init__(self, tile_no, x_pos, y_pos):
+    def __init__(self, tile_no, x_pos, y_pos, color):
         self.tile_no = tile_no
         self.x_pos = x_pos
         self.y_pos = y_pos
-       
-    def draw(self, color):
-        # print(color, self.tile_no, self.x_pos, self.y_pos)
-        pg.draw.rect(screen, color,
+        self.color = color
+
+    def check_win(self):
+        global check, poor
+        check = False
+        poor = False
+        check_data= 0
+        for index in range(1,BOARD_SIZE_X * BOARD_SIZE_Y):
+            if index == tiles[index-1]:
+                check_data += 1
+            print (index, tiles[index-1])
+        print (check_data, BOARD_SIZE_X * BOARD_SIZE_Y -1)
+        if check_data == BOARD_SIZE_X * BOARD_SIZE_Y -1 :
+            check = True  
+        elif check_data == BOARD_SIZE_X * BOARD_SIZE_Y -3 and \
+            tiles[BOARD_SIZE_X * BOARD_SIZE_Y-2]== BOARD_SIZE_X * BOARD_SIZE_Y-1 and\
+            tiles[BOARD_SIZE_X * BOARD_SIZE_Y-1]== BOARD_SIZE_X * BOARD_SIZE_Y-2 :
+            poor = True
+        
+        return check, poor
+
+    def draw(self):
+        # print(color, self.self.tile_no, self.x_pos, self.y_pos)
+        pg.draw.rect(screen, self.color,
                  (self.x_pos * CELL_SIZE, self.y_pos * CELL_SIZE,
                    CELL_SIZE,CELL_SIZE))    
         NO_TAG = game_font.render(str(tiles[self.tile_no]), True, BLACK)
                     # 출력할 글자, 안티 알리아스  True, 글자 색상
         screen.blit(NO_TAG, (int((self.x_pos +0.2) * CELL_SIZE),int((self.y_pos+0.2) * CELL_SIZE)))
-        # # print(NO_TAG, self.x_pos,self.y_pos)
+       
             
-    def move(self, tile_no):
+    def move(self):
 
-        if tiles[tile_no] != 0 : 
+        if tiles[self.tile_no] != 0 : 
             #top left
             if self.x_pos == 0 and self.y_pos == 0 :     
-                if tiles[tile_no+1] == 0:                     # 오른쪽 0  
-                    tiles[tile_no],tiles[tile_no+1]= tiles[tile_no+1],tiles[tile_no] 
-                elif tiles[tile_no + BOARD_SIZE_X] == 0 :           # 아래 0
-                    tiles[tile_no], tiles[tile_no + BOARD_SIZE_X] = tiles[tile_no + BOARD_SIZE_X], tiles[tile_no]
+                if tiles[self.tile_no+1] == 0:                     # 오른쪽 0  
+                    tiles[self.tile_no],tiles[self.tile_no+1]= tiles[self.tile_no+1],tiles[self.tile_no] 
+                elif tiles[self.tile_no + BOARD_SIZE_X] == 0 :           # 아래 0
+                    tiles[self.tile_no], tiles[self.tile_no + BOARD_SIZE_X] = tiles[self.tile_no + BOARD_SIZE_X], tiles[self.tile_no]
             #top
             elif self.x_pos > 0 and self.x_pos < BOARD_SIZE_X -1 and self.y_pos == 0:
-                if tiles[tile_no-1] == 0:        # 왼쪽   0
-                     tiles[tile_no],tiles[tile_no-1]= tiles[tile_no-1],tiles[tile_no]
-                elif tiles[tile_no+BOARD_SIZE_X] == 0 :           # 아래 0
-                    tiles[tile_no],tiles[tile_no+BOARD_SIZE_X]= tiles[tile_no+BOARD_SIZE_X],tiles[tile_no]
-                elif tiles[tile_no+1] == 0:                     # 오른쪽 0  
-                    tiles[tile_no],tiles[tile_no+1]= tiles[tile_no+1],tiles[tile_no]
+                if tiles[self.tile_no-1] == 0:        # 왼쪽   0
+                     tiles[self.tile_no],tiles[self.tile_no-1]= tiles[self.tile_no-1],tiles[self.tile_no]
+                elif tiles[self.tile_no+BOARD_SIZE_X] == 0 :           # 아래 0
+                    tiles[self.tile_no],tiles[self.tile_no+BOARD_SIZE_X]= tiles[self.tile_no+BOARD_SIZE_X],tiles[self.tile_no]
+                elif tiles[self.tile_no+1] == 0:                     # 오른쪽 0  
+                    tiles[self.tile_no],tiles[self.tile_no+1]= tiles[self.tile_no+1],tiles[self.tile_no]
             #top right
             if self.x_pos == BOARD_SIZE_X-1 and self.y_pos == 0 :     
-                if tiles[tile_no-1] == 0:                     # 왼쪽 0  
-                    tiles[tile_no],tiles[tile_no-1]= tiles[tile_no-1],tiles[tile_no]
-                elif tiles[tile_no+BOARD_SIZE_X] == 0 :           # 아래 0
-                    tiles[tile_no],tiles[tile_no+BOARD_SIZE_X]= tiles[tile_no+BOARD_SIZE_X],tiles[tile_no]
+                if tiles[self.tile_no-1] == 0:                     # 왼쪽 0  
+                    tiles[self.tile_no],tiles[self.tile_no-1]= tiles[self.tile_no-1],tiles[self.tile_no]
+                elif tiles[self.tile_no+BOARD_SIZE_X] == 0 :           # 아래 0
+                    tiles[self.tile_no],tiles[self.tile_no+BOARD_SIZE_X]= tiles[self.tile_no+BOARD_SIZE_X],tiles[self.tile_no]
             #left
             elif self.x_pos == 0  and self.y_pos > 0 and self.y_pos < BOARD_SIZE_Y -1:
-                if tiles[tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
-                     tiles[tile_no],tiles[tile_no-BOARD_SIZE_X]= tiles[tile_no-BOARD_SIZE_X],tiles[tile_no]
-                elif tiles[tile_no+BOARD_SIZE_X] == 0 :           # 아래 0
-                    tiles[tile_no],tiles[tile_no+BOARD_SIZE_X]= tiles[tile_no+BOARD_SIZE_X],tiles[tile_no]
-                elif tiles[tile_no+1] == 0:                     # 오른쪽 0  
-                    tiles[tile_no],tiles[tile_no+1]= tiles[tile_no+1],tiles[tile_no]
+                if tiles[self.tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
+                     tiles[self.tile_no],tiles[self.tile_no-BOARD_SIZE_X]= tiles[self.tile_no-BOARD_SIZE_X],tiles[self.tile_no]
+                elif tiles[self.tile_no+BOARD_SIZE_X] == 0 :           # 아래 0
+                    tiles[self.tile_no],tiles[self.tile_no+BOARD_SIZE_X]= tiles[self.tile_no+BOARD_SIZE_X],tiles[self.tile_no]
+                elif tiles[self.tile_no+1] == 0:                     # 오른쪽 0  
+                    tiles[self.tile_no],tiles[self.tile_no+1]= tiles[self.tile_no+1],tiles[self.tile_no]
             #center
             elif self.x_pos > 0 and self.x_pos < BOARD_SIZE_X -1 and self.y_pos > 0 and self.y_pos < BOARD_SIZE_Y-1 :
-                if tiles[tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
-                     tiles[tile_no],tiles[tile_no-BOARD_SIZE_X]= tiles[tile_no-BOARD_SIZE_X],tiles[tile_no]
-                elif tiles[tile_no+BOARD_SIZE_X] == 0 :           # 아래 0
-                    tiles[tile_no],tiles[tile_no+BOARD_SIZE_X]= tiles[tile_no+BOARD_SIZE_X],tiles[tile_no]
-                elif tiles[tile_no+1] == 0:                     # 오른쪽 0  
-                    tiles[tile_no],tiles[tile_no+1]= tiles[tile_no+1],tiles[tile_no]    
-                elif tiles[tile_no-1] == 0:                     # 왼쪽 0  
-                    tiles[tile_no],tiles[tile_no-1]= tiles[tile_no-1],tiles[tile_no]    
+                if tiles[self.tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
+                     tiles[self.tile_no],tiles[self.tile_no-BOARD_SIZE_X]= tiles[self.tile_no-BOARD_SIZE_X],tiles[self.tile_no]
+                elif tiles[self.tile_no+BOARD_SIZE_X] == 0 :           # 아래 0
+                    tiles[self.tile_no],tiles[self.tile_no+BOARD_SIZE_X]= tiles[self.tile_no+BOARD_SIZE_X],tiles[self.tile_no]
+                elif tiles[self.tile_no+1] == 0:                     # 오른쪽 0  
+                    tiles[self.tile_no],tiles[self.tile_no+1]= tiles[self.tile_no+1],tiles[self.tile_no]    
+                elif tiles[self.tile_no-1] == 0:                     # 왼쪽 0  
+                    tiles[self.tile_no],tiles[self.tile_no-1]= tiles[self.tile_no-1],tiles[self.tile_no]    
             #right
             elif self.x_pos == BOARD_SIZE_X -1 and self.y_pos > 0 and self.y_pos < BOARD_SIZE_Y-1 :
-                if tiles[tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
-                     tiles[tile_no],tiles[tile_no-BOARD_SIZE_X]= tiles[tile_no-BOARD_SIZE_X],tiles[tile_no]
-                elif tiles[tile_no+BOARD_SIZE_X] == 0 :           # 아래 0
-                    tiles[tile_no],tiles[tile_no+BOARD_SIZE_X]= tiles[tile_no+BOARD_SIZE_X],tiles[tile_no]
-                elif tiles[tile_no-1] == 0:                     # 왼쪽 0  
-                    tiles[tile_no],tiles[tile_no-1]= tiles[tile_no-1],tiles[tile_no]    
+                if tiles[self.tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
+                     tiles[self.tile_no],tiles[self.tile_no-BOARD_SIZE_X]= tiles[self.tile_no-BOARD_SIZE_X],tiles[self.tile_no]
+                elif tiles[self.tile_no+BOARD_SIZE_X] == 0 :           # 아래 0
+                    tiles[self.tile_no],tiles[self.tile_no+BOARD_SIZE_X]= tiles[self.tile_no+BOARD_SIZE_X],tiles[self.tile_no]
+                elif tiles[self.tile_no-1] == 0:                     # 왼쪽 0  
+                    tiles[self.tile_no],tiles[self.tile_no-1]= tiles[self.tile_no-1],tiles[self.tile_no]    
             #bottom left
             elif self.x_pos  ==0  and self.y_pos == BOARD_SIZE_Y-1 :
-                if tiles[tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
-                     tiles[tile_no],tiles[tile_no-BOARD_SIZE_X]= tiles[tile_no-BOARD_SIZE_X],tiles[tile_no]
-                elif tiles[tile_no+1] == 0:                     # 오른쪽 0  
-                    tiles[tile_no],tiles[tile_no+1]= tiles[tile_no+1],tiles[tile_no]     
+                if tiles[self.tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
+                     tiles[self.tile_no],tiles[self.tile_no-BOARD_SIZE_X]= tiles[self.tile_no-BOARD_SIZE_X],tiles[self.tile_no]
+                elif tiles[self.tile_no+1] == 0:                     # 오른쪽 0  
+                    tiles[self.tile_no],tiles[self.tile_no+1]= tiles[self.tile_no+1],tiles[self.tile_no]     
             #bottom
             elif self.x_pos > 0 and self.x_pos < BOARD_SIZE_X -1 and self.y_pos == BOARD_SIZE_Y-1 :
-                if tiles[tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
-                     tiles[tile_no],tiles[tile_no-BOARD_SIZE_X]= tiles[tile_no-BOARD_SIZE_X],tiles[tile_no]
-                elif tiles[tile_no+1] == 0:                     # 오른쪽 0  
-                    tiles[tile_no],tiles[tile_no+1]= tiles[tile_no+1],tiles[tile_no]    
-                elif tiles[tile_no-1] == 0:                     # 왼쪽 0  
-                    tiles[tile_no],tiles[tile_no-1]= tiles[tile_no-1],tiles[tile_no]
+                if tiles[self.tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
+                     tiles[self.tile_no],tiles[self.tile_no-BOARD_SIZE_X]= tiles[self.tile_no-BOARD_SIZE_X],tiles[self.tile_no]
+                elif tiles[self.tile_no+1] == 0:                     # 오른쪽 0  
+                    tiles[self.tile_no],tiles[self.tile_no+1]= tiles[self.tile_no+1],tiles[self.tile_no]    
+                elif tiles[self.tile_no-1] == 0:                     # 왼쪽 0  
+                    tiles[self.tile_no],tiles[self.tile_no-1]= tiles[self.tile_no-1],tiles[self.tile_no]
             #bottom right
             elif self.x_pos== BOARD_SIZE_X -1 and self.y_pos == BOARD_SIZE_Y-1 :
-                if tiles[tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
-                     tiles[tile_no],tiles[tile_no-BOARD_SIZE_X]= tiles[tile_no-BOARD_SIZE_X],tiles[tile_no]
-                elif tiles[tile_no-1] == 0:                     # 왼쪽 0  
-                    tiles[tile_no],tiles[tile_no-1]= tiles[tile_no-1],tiles[tile_no]    
+                if tiles[self.tile_no-BOARD_SIZE_X] == 0:        # 위쪽   0
+                     tiles[self.tile_no],tiles[self.tile_no-BOARD_SIZE_X]= tiles[self.tile_no-BOARD_SIZE_X],tiles[self.tile_no]
+                elif tiles[self.tile_no-1] == 0:                     # 왼쪽 0  
+                    tiles[self.tile_no],tiles[self.tile_no-1]= tiles[self.tile_no-1],tiles[self.tile_no]    
           
 
 def draw_grid():
@@ -143,32 +164,44 @@ def runGame():
                 x_pos = event.pos[0] // CELL_SIZE
                 y_pos = event.pos[1] // CELL_SIZE
                 tile_no = x_pos + y_pos * BOARD_SIZE_Y
-                tile= Tiles(tile_no, x_pos, y_pos)
+                if tile_no == 0:
+                        color = BLACK
+                else :
+                        color = WHITE    
+                tile= Tiles(tile_no, x_pos, y_pos, color)
                 
                 # print (tiles)
 
-                tile.move(tile_no)
-                
-                # print (tiles)
+                tile.move()
+                tile.check_win()
+                print(check)
+                print(poor)
 
-                for index in range(len(tiles)):
-                
+                for index in range(len(tiles)):               
                     tile_no= tiles[index]
-                    # print( tile_no)
+                    # print( self.tile_no)
                     x_pos = index  % BOARD_SIZE_X
                     y_pos = index  // BOARD_SIZE_Y
-                    tile= Tiles(index, x_pos, y_pos)
                     if tile_no == 0:
                         color = BLACK
                     else :
                         color = WHITE    
-                    tile.draw(color) 
-               
-                # print (x_pos, y_pos)
-            # print('////////////////') 
-            # print(tiles)   
-            # print(len(tiles))
+
+                    tile= Tiles(index, x_pos, y_pos, color)
+                    tile.draw() 
                     draw_grid()
+
+                    if check :
+                        win_msg = game_font.render('Good!!!', True, RED)
+                    # 출력할 글자, 안티 알리아스  True, 글자 색상
+                        screen.blit(win_msg, (SCREEN_WIDTH // 5,SCREEN_HEIGHT //3 ))
+                        # print (tiles)
+                    elif poor:
+                        win_msg = game_font.render('Oh No!!!', True, RED)
+                    # 출력할 글자, 안티 알리아스  True, 글자 색상
+                        screen.blit(win_msg, (SCREEN_WIDTH // 5,SCREEN_HEIGHT //3 ))
+                        # print (tiles)
+
                 pg.display.update()
             
 if __name__ == '__main__':                  
