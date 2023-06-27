689636227e4b54987e8d3f71439b51a551c14604
DESKTOP-OK2D2LF\donge <dongeal@gmail.com>
2023-06-27 20:25:56 +0900

    no message

--- tilts02.py
+++ tilts02.py
@@ -1,16 +1,16 @@
 import pygame as pg # 1. 파이게임 불러오기
-from random import  shuffle
+from random import  randint
 
 pg.init()           # 2. 초기화
 
-
+SCRAMBLE_NO= 100
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (128,128,128)
 RED = (255,0,0)
 CELL_SIZE = 100
-BOARD_SIZE_X = 3
-BOARD_SIZE_Y = 3
+BOARD_SIZE_X = 4
+BOARD_SIZE_Y = 4
 
 SCREEN_WIDTH = CELL_SIZE * BOARD_SIZE_X
 SCREEN_HEIGHT = CELL_SIZE * BOARD_SIZE_Y
@@ -27,14 +27,15 @@ done = False
 
 # 타일 랜덤 작업
 tiles = []
-for index in range(0, BOARD_SIZE_X * BOARD_SIZE_Y):
-    tiles.append(index)  # [0, 1, 2, ....., 15]
-
+for index in range(1, BOARD_SIZE_X * BOARD_SIZE_Y ):
+    tiles.append(index)  # [1, 2, ....., 15]
+tiles.append(0)         # [1, 2, ....., 15,0]
+print(tiles)
 # tiles =[x for x in range(COLUMN_COUNT * BOARD_SIZE_X)] 
 # tile_colors = {0:BLACK}
 # for index in range(0, COLUMN_COUNT * BOARD_SIZE_X):
 #     tile_colors[index]= WHITE
-shuffle(tiles)
+# shuffle(tiles)
 
 class Tiles :
     def __init__(self, tile_no, x_pos, y_pos, color):
@@ -42,7 +43,35 @@ class Tiles :
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.color = color
+    
+    def scramble(self):
+        for index in range(1,SCRAMBLE_NO):
+            x_pos = randint(0, BOARD_SIZE_X-1)
+            y_pos = randint(0, BOARD_SIZE_Y-1)
+            tile_no = x_pos + y_pos * BOARD_SIZE_Y
+            if tile_no == 0:
+                    color = BLACK
+            else :
+                    color = WHITE    
+            tile= Tiles(tile_no, x_pos, y_pos, color)
+            tile.move()
+            
+            for index in range(len(tiles)):               
+                tile_no= tiles[index]
+                # print( self.tile_no)
+                x_pos = index  % BOARD_SIZE_X
+                y_pos = index  // BOARD_SIZE_Y
+                if tile_no == 0:
+                    color = BLACK
+                else :
+                    color = WHITE    
 
+                tile= Tiles(index, x_pos, y_pos, color)
+                tile.draw() 
+                draw_grid()
+          
+                pg.display.update()
+            clock.tick(30)
     def check_win(self):
         global check, poor
         check = False
@@ -104,7 +133,23 @@ def draw_grid():
 
 def runGame():
     global done
-   
+
+    for index in range(len(tiles)):               
+        tile_no= tiles[index]
+        # print( self.tile_no)
+        x_pos = index  % BOARD_SIZE_X
+        y_pos = index  // BOARD_SIZE_Y
+        if tile_no == 0:
+            color = BLACK
+        else :
+            color = WHITE    
+
+        tile= Tiles(index, x_pos, y_pos, color)
+        tile.draw() 
+        draw_grid()
+    pg.display.update()
+    tile.scramble()
+
     while not done:
         clock.tick(10)
         screen.fill(WHITE)
